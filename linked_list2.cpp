914814f1788dd5fda56392de2fc3c708bc56efb0
Jason Narvaez <thejason@tamu.edu>
2015-09-20 16:57:55 -0500

    Update and rename linked_list2.c to linked_list2.cpp

---
+++ linked_list2.cpp
@@ -0,0 +1,143 @@
+#include "linked_list2.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void 	Init (int M, int b, int t){
+	
+	//Tiers = (MemoryPool*) malloc(t);
+	tier_t = t;//global variable for t
+	
+	MemoryPool temp;
+	for(int i=0;i<t;++i){
+		Tiers.push_back(temp);
+		
+	}
+	
+	
+	int i = 0;
+	for(i = 0; i<t;++i){
+		Tiers[i].mempool = (Node*) malloc(M);
+		Tiers[i].head = Tiers[i].mempool;
+		Tiers[i].free_pointer = Tiers[i].head;//nothing is in the first node
+		Tiers[i].set_b(b);
+		Tiers[i].set_M(M);
+		Tiers[i].trailer = Tiers[i].head + Tiers[i].get_M();
+		
+		/* current->mempool = (Node*) malloc(M);
+		current->head = current->mempool;
+		current->free_pointer = current->head;//nothing is in the first node
+		current->set_b(b);
+		current->set_M(M);
+		current->trailer = current->head + current->get_M();
+		 */
+		//current->free_pointer->next = current->free_pointer + b;
+		
+		//if(i<t-1)current++;
+		tier_size++;
+		
+	}
+	//current = Tiers;
+	for(i = 0; i<t;++i){
+		//printf("current: %d\n",current);
+		printf("current free pointer: %d\n",Tiers[i].free_pointer);
+		printf("current free pointer next: %d\n",Tiers[i].free_pointer->next);
+		//if(i<t-1)current++;
+		
+	}
+	
+	
+} 
+void 	Destroy (){} 		 
+int 	Insert (int key,char * value_ptr, int value_len){
+	
+	
+	int max_size = INT_MAX;//maximum size of a signed integer
+	//printf("max size: %u\n",max_size);
+	
+	
+	printf("key: %u\n",key);
+	
+	int tier_range = max_size/tier_t;//specifies the key range for each tier. tier t is the amt of tiers
+	
+	//printf("tier range: %u\n",tier_range);
+	
+	int tier_placement = ((unsigned int)key)/tier_range;//tier where node will be place
+	
+	printf("tier placement: %u\n",tier_placement);
+	
+	//if(tier_placement >= 0) printf("TRUE1\n");
+	//if(tier_placement < tier_t) printf("TRUE2\n");
+	if(tier_placement >= 0 &&  tier_placement < tier_t){//needs to check for available space within tier
+	
+		MemoryPool current = Tiers[0];//point to the first tier
+		
+		//printf("\nb: %d\n",current->b);
+		//printf("current free pointer: %d\n",current.free_pointer);
+		//printf("current free pointer next: %d\n",current.free_pointer->next);
+		
+		current = Tiers[tier_placement];//current now points to the tier where we will insert the node
+		//printf("tier: %d\n",tier_placement);
+		//printf("b: %d\n",current->b);
+		
+		if(value_len<=current.get_b()){
+			char temp[current.get_b()-16];
+			printf("before key assignment\n\n");
+			//if(current->free_pointer) printf("free pointer not NULL!\n\n");
+			
+			
+			current.free_pointer->key = key;//segfault
+			current.free_pointer->value_len = value_len;
+			printf("done deed\n");
+			
+			current.free_pointer->value = temp;
+			
+			memcpy(current.free_pointer->value,value_ptr, value_len);//copy value_len elements from value_ptr to MP.value
+			//printf("b: %d\n",current.get_b());
+			
+			//printf("current free pointer: %d\n",current.free_pointer);
+			//printf("current free pointer next: %d\n",current.free_pointer->next);
+			
+			current.free_pointer->next = current.free_pointer + current.get_b();
+			
+			// printf("b: %d\n",current.get_b());
+			// printf("current free pointer: %d\n",current.free_pointer);
+			// printf("current free pointer next: %d\n",current.free_pointer->next);
+			
+			current.free_pointer = current.free_pointer->next;//current->b;//MP.b;
+			
+			// printf("b: %d\n",current.get_b());
+			// printf("current free pointer: %d\n",current.free_pointer);
+			// printf("current free pointer next: %d\n",current.free_pointer->next);
+		}
+		else{
+			printf("Value Length Too Large\n");
+			
+		}
+		//if(current.free_pointer)printf("free_pointer not NULL\n\n");
+		// printf("inserted: %d\n",*value_ptr);
+		// printf("inserted: %s\n",value_ptr);
+		
+	}
+	else{
+		printf("key is too large or key is negative\n");
+	}
+
+}
+int 	Delete (int key){}
+char* 	Lookup (int key){}
+void 	PrintList (){
+	
+	//MemoryPool* current = Tiers;//start at the beginning
+	
+	int i = 0;
+	for(i = 0;i<tier_size;++i){
+		//printf("i: %d\n",i);
+		
+	}
+	//int t = sizeof(Tiers);
+	printf("size of tiers: %d",tier_size);
+
+
+
+}
+
