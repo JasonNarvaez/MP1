6888d740db0dea4579a3250eaf29c23713ef78e5
hcheng761 <zhestalogy@tamu.edu>
2015-09-20 01:51:02 -0500

    destroy COMPLETE!

--- linked_list2.c
+++ linked_list2.c
@@ -2,31 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void 	Init (int M, int b, int t){
-	Tiers = (MemoryPool*) malloc(t);
-	
+void Init(int M, int b, int t){
+	Tiers = (MemoryPool*)malloc(t);
+
 	MemoryPool* current = Tiers;//start at the beginning
-	
+
 	int i = 0;
-	for(i = 0; i<t;++i){
-		current->mempool = (Node*) malloc(M);
+	for (i = 0; i<t; ++i){
+		current->mempool = (Node*)malloc(M);
 		current->head = current->mempool;
 		current->free_pointer = current->head;//nothing is in the first node
-		current->b=b;
-		current->M=M;
+		current->b = b;
+		current->M = M;
 		current->trailer = current->head + M;
-		
+
 		current++;
-		
+
 	}
+}
+void Destroy()	
+{
+	for (int i = 0; i < tiersize; i++)
+	{	//array of memory pools
+		MemoryPool* t = Tiers[i]
+		Node *temp2 = t.head->next->next;
+		while (t.head->next != NULL)
+		{
+			Node *temp;
+			temp = t.head->next;
+			t.head->next = temp2;
+			free(temp);
+		}
+		free(t.head);
+		free(t);
+	}
+}
+int Insert(int key, char * value_ptr, int value_len){}
+int Delete(int key){}
+char* Lookup(int key)
+{
 	
-	
-
-
-} 
-void 	Destroy (){} 		 
-int 	Insert (int key,char * value_ptr, int value_len){}
-int 	Delete (int key){}
-char* 	Lookup (int key){}
-void 	PrintList (){}
-
+}
+void PrintList(){}
