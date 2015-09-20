d2a14a816bf2a10aafefbea45c80569f647546e2
Jason Narvaez <thejason@tamu.edu>
2015-09-20 12:44:44 -0500

    Update linked_list2.c

--- linked_list2.c
+++ linked_list2.c
@@ -38,7 +38,64 @@ void Destroy()
 		free(t);
 	}
 }
-int Insert(int key, char * value_ptr, int value_len){}
+int Insert(int key, char * value_ptr, int value_len){
+	
+	unsigned int max_size = INT_MAX;//maximum size of a signed integer
+	printf("max size: %u\n",max_size);
+	
+	
+	printf("key: %u\n",key);
+	
+	unsigned int tier_range = max_size/tier_t;//specifies the key range for each tier. tier t is the amt of tiers
+	
+	printf("tier range: %u\n",tier_range);
+	
+	unsigned int tier_placement = ((unsigned int)key)/tier_range;//tier where node will be place
+	
+	printf("tier placement: %u\n",tier_placement);
+	
+	if(tier_placement >= 0) printf("TRUE1\n");
+	if(tier_placement < tier_t) printf("TRUE2\n");
+	if(tier_placement >= 0 &&  tier_placement < tier_t){//needs to check for available space within tier
+	
+		MemoryPool* current = Tiers;//point to the first tier
+		printf("\nb: %d\n",current->b);
+		current += tier_placement;//current now points to the tier where we will insert the node
+		printf("tier: %d\n",tier_placement);
+		printf("b: %d\n",current->b);
+		
+		if(value_len<=current->b){
+			char temp[current->b-16];
+			printf("before key assignment\n\n");
+			//if(current->free_pointer) printf("free pointer not NULL!\n\n");
+			
+			
+			current->free_pointer->key = key;//segfault
+			current->free_pointer->value_len = value_len;
+			printf("done deed\n");
+			
+			current->free_pointer->value = temp;
+			
+			memcpy(current->free_pointer->value,value_ptr, value_len);//copy value_len elements from value_ptr to MP.value
+			
+			current->free_pointer->next += current->b;
+			current->free_pointer += current->b;//MP.b;
+			printf("b: %d\n",current->b);
+		}
+		else{
+			printf("Value Length Too Large\n");
+			
+		}
+		if(current->free_pointer)printf("free_pointer not NULL\n\n");
+		// printf("inserted: %d\n",*value_ptr);
+		// printf("inserted: %s\n",value_ptr);
+		
+	}
+	else{
+		printf("key is too large or key is negative\n");
+	}
+	
+}
 int Delete(int key){}
 char* Lookup(int key)
 {
