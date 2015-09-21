21bb5d5ec03f76367b903759aa6e83d849271449
Jason Narvaez <thejason@tamu.edu>
2015-09-20 20:19:33 -0500

    Update linked_list2.h

--- linked_list2.h
+++ linked_list2.h
@@ -2,6 +2,7 @@
 #define _linked_list2_h_
 #include <limits.h>
 #include <vector>
+#include <iostream>
 
 void 	Init (int M, int b, int t); // initializes the linked list, should be called once from the main
 void 	Destroy (); 		 //	destroys the linked list and cleans resources
@@ -11,7 +12,7 @@ char* 	Lookup (int key);	 //	Looks up the first item with the given and returns 
 							//(the user can read or modify after obtaining the pointer)
 void 	PrintList ();		// prints the entire list by following the next pointers. Print the keys and the length	of the value
 
-
+using namespace std;
 
 typedef struct Node Node;
 typedef struct MemoryPool MemoryPool;
